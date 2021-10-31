82a239dc90fdd4f57f27e6b10f790a4a25522f35
Outlander <sujilgovind@users.noreply.github.com>
2021-10-31 14:43:26 +0530

    added express server changes

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const express = require('express');
 const PORT = 3002;
+const app = express();
+
+const server = app.listen(PORT, ()=>{
+	const host = server.address().remoteAddress
+	console.log('App listening to http://%s:%s',host,PORT) 
+});
